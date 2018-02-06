c102e01ae14bd7d848c62f6180fd3f0669aada76
anton1gritsenko <33102708+anton1gritsenko@users.noreply.github.com>
2018-02-06 19:04:19 +0200

    Update url.js

--- app/factories/url.js
+++ app/factories/url.js
@@ -8,7 +8,7 @@
     url.$inject = [];
 
     function url() {
-        let baseUrl = 'https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/';
+       /* let baseUrl = 'https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/';*/
           let baseUrl = 'https://anton1gritsenko.github.io/weather-app/';
         return {
             baseUrl: baseUrl
